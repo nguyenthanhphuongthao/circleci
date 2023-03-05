d12019d68191afbcc5f14d1c46c7801b92d5f803
unknown <20110197@student.hcmute.edu.vn>
2023-03-05 21:19:24 +0700

    Adds test 2

--- main-test.py
+++ main-test.py
@@ -2,7 +2,7 @@ from main import Add
 
 def TestAdd():
         assert Add(2,3) == 5
-        assert Add(5,5) == 11
+        assert Add(5,5) == 10
         print("Hàm Add() hoạt động đúng")
 
 if __name__ == '__main__':
